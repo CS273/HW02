4873a5192a828d5e71f7f364e7a4079158764b2a
joronia <jaoronia@gmail.com>
2015-02-13 18:30:40 -0800

    Fixed class

--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -55,22 +55,26 @@ public:
 private:
 	class Directory_Entry /* Exercise 1.6: Please complete the definition of the Directory_Entry class here. - Ed/Kent */
 	{
+	
 	public:
 		Directory_Entry() {} // Default no-argument constructor 
 		Directory_Entry(std::string the_name, std::string the_number) {
+			name = the_name;
+			number = the_number;
 			// constructor not implemented yet
 		}
 		std::string get_name() const {
-			return ""; // method not implemented yet
+			return name; // method not implemented yet
 		}
 		std::string get_number() const {
-			return ""; // method not implemented yet
+			return number; // method not implemented yet
 		}
 		void set_number(const std::string& new_number) {
-			// method not implemented yet
+			number = new_number; // method not implemented yet
 		}
 	private:
-
+		std::string name;
+		std::string number;
 	};
 
 	// Private Functions
